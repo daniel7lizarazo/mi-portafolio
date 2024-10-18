5e583ef4f91516047ca14706a884cd31b4a0508e
dajuf7 <daniel7lizarazo@gmail.com>
2024-10-18 17:20:41 -0500

    Nombre mejorado animación

--- content/content.js
+++
@@ -1,5 +0,0 @@
-import { APP_CONTENT_TAG_NAME, AppContentComponent } from "./content.component";
-
-if (customElements.get(APP_CONTENT_TAG_NAME) === undefined) {
-  customElements.define(APP_CONTENT_TAG_NAME, AppContentComponent);
-}
